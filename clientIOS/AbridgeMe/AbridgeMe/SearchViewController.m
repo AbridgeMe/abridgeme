069a855032ef077d281d440bf04aaad76a597cdd
Dima Vartanian <dim1014@gmail.com>
2015-07-09 19:48:56 -0400

    remove scroll animation and delete a bunch more useless code

--- clientIOS/AbridgeMe/AbridgeMe/SearchViewController.m
+++ clientIOS/AbridgeMe/AbridgeMe/SearchViewController.m
@@ -49,11 +49,6 @@
 
 @synthesize tblviewArticle,allTopic;
 
-
-@synthesize cellZoomXScaleFactor = _xZoomFactor;
-@synthesize cellZoomYScaleFactor = _yZoomFactor;
-@synthesize cellZoomAnimationDuration = _animationDuration;
-@synthesize cellZoomInitialAlpha = _initialAlpha;
 @synthesize allDataArray,allHashArray;
 
 
@@ -951,108 +946,6 @@
     for (CustomArticleCell *cell in visibleCells) {
         [cell cellOnTableView:self.tblviewArticle didScrollOnView:self.view];
     }
-}
-
-
-
-
-// Cell Animation //
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
-    
-    if ((indexPath.section == 0 && currentMaxDisplayedCell == 0) || indexPath.section > currentMaxDisplayedSection){ //first item in a new section, reset the max row count
-        currentMaxDisplayedCell = -1;
-    }
-    
-    if (indexPath.section >= currentMaxDisplayedSection && indexPath.row > currentMaxDisplayedCell){
-        //now make the image view a bit bigger, so we can do a zoomout effect when it becomes visible
-        cell.contentView.alpha = self.cellZoomInitialAlpha.floatValue;
-        
-        CGAffineTransform transformScale = CGAffineTransformMakeScale(self.cellZoomXScaleFactor.floatValue, self.cellZoomYScaleFactor.floatValue);
-        CGAffineTransform transformTranslate = CGAffineTransformMakeTranslation(self.cellZoomXOffset.floatValue, self.cellZoomYOffset.floatValue);
-        
-        cell.contentView.transform = CGAffineTransformConcat(transformScale, transformTranslate);
-        
-        [self.tblviewArticle bringSubviewToFront:cell.contentView];
-        [UIView animateWithDuration:self.cellZoomAnimationDuration.floatValue
-                              delay:0
-                            options:UIViewAnimationOptionAllowUserInteraction
-                         animations:^{
-                             cell.contentView.alpha = 1;
-                             //clear the transform
-                             cell.contentView.transform = CGAffineTransformIdentity;
-                         } completion:nil];
-        
-        
-        currentMaxDisplayedCell = (int)indexPath.row;
-        currentMaxDisplayedSection = (int)indexPath.section;
-    }
-}
-
-
--(void)resetViewedCells{
-    currentMaxDisplayedSection = 0;
-    currentMaxDisplayedCell = 0;
-}
-
-#pragma -mark Setters for four customisable variables
--(void)setCellZoomXScaleFactor:(NSNumber *)xZoomFactor{
-    _xZoomFactor = xZoomFactor;
-}
--(void)setCellZoomYScaleFactor:(NSNumber *)yZoomFactor{
-    _yZoomFactor = yZoomFactor;
-}
--(void)setCellZoomAnimationDuration:(NSNumber *)animationDuration{
-    _animationDuration = animationDuration;
-}
--(void)setCellZoomInitialAlpha:(NSNumber *)initialAlpha{
-    _initialAlpha = initialAlpha;
-}
-
-#pragma -mark Getters for four customisable variable. Provide default if not set.
--(NSNumber *)cellZoomXScaleFactor{
-    if (_xZoomFactor == nil){
-        _xZoomFactor = [NSNumber numberWithFloat:1.25];
-    }
-    return _xZoomFactor;
-}
--(NSNumber *)cellZoomXOffset{
-    if (_cellZoomXOffset == nil){
-        _cellZoomXOffset = [NSNumber numberWithFloat:0];
-    }
-    return _cellZoomXOffset;
-}
--(NSNumber *)cellZoomYOffset{
-    if (_cellZoomYOffset == nil){
-        _cellZoomYOffset = [NSNumber numberWithFloat:0];
-    }
-    return _cellZoomYOffset;
-}
--(NSNumber *)cellZoomYScaleFactor{
-    if (_yZoomFactor == nil){
-        _yZoomFactor = [NSNumber numberWithFloat:1.25];
-    }
-    return _yZoomFactor;
-}
--(NSNumber *)cellZoomAnimationDuration{
-    if (_animationDuration == nil){
-        _animationDuration = [NSNumber numberWithFloat:0.65];
-    }
-    return _animationDuration;
-}
--(NSNumber *)cellZoomInitialAlpha{
-    if (_initialAlpha == nil){
-        _initialAlpha = [NSNumber numberWithFloat:0.3];
-    }
-    return _initialAlpha;
-}
-
-
-
-
-- (void) viewDidUnload
-{
-    //[self releaseViewComponents];
-    [super viewDidUnload];
 }
 
 - (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
